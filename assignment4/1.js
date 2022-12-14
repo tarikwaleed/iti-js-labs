4350ce31d03b0de117236942ef75b4b03030f4be
tarikwaleed <takizee53@gmail.com>
2022-12-14 20:56:05 +0200

    :bug: assignment4/1.js

--- assignment4/1.js
+++ assignment4/1.js
@@ -1,23 +1,24 @@
-var fadeDegree=1
 function fadeOut(image) {
+    var fadeDegree = 1.0
     var interval = setInterval(() => {
-        if (fadeDegree <= 0) {
+        if (fadeDegree <= 0.0) {
             clearInterval(interval)
         }
-        image.setAttribute("style", `opacity:${fadeDegree}`)
-        image.style.opacity=fadeDegree
+        // image.setAttribute("style", `opacity:${fadeDegree}`)
+        image.style.opacity = fadeDegree
         fadeDegree -= 0.1;
 
     }, 1000);
 }
 
 function fadeIn(image) {
+    var fadeDegree = 0.0
     var interval = setInterval(() => {
-        if (fadeDegree >= 100) {
+        if (fadeDegree >= 1) {
             clearInterval(interval)
         }
         // image.setAttribute("style", `opacity:${fadeDegree/100}`)
-        image.style.opacity=fadeDegree;
+        image.style.opacity = fadeDegree;
         fadeDegree += .1;
 
     }, 1000);
