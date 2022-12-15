3a70d9532803f201384824b4b7bd7ee35d709448
tarikwaleed <takizee53@gmail.com>
2022-12-15 16:17:33 +0200

    :hammer:->5.js:got data from the api and populated it successfully to select element

--- assignment5/5.js
+++ assignment5/5.js
@@ -1,7 +1,6 @@
 function GetUser() {
     var userIdInputField = document.getElementById("user-id-input-field")
     var userId = userIdInputField.value
-    console.log(userId)
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.status == 200 && this.readyState == 4) {
@@ -14,3 +13,26 @@ function GetUser() {
     xhttp.open("GET", `https://reqres.in/api/users/${userId}`, true);
     xhttp.send();
 }
+
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (this.status == 200 && this.readyState == 4) {
+        var allUsers = JSON.parse(this.responseText).data
+        console.log(allUsers)
+        var selectElement = document.createElement('select'),
+            optionElement;
+
+        for (let i = 0; i < allUsers.length; i++) {
+            optionElement = document.createElement('option');
+            optionElement.setAttribute('value', allUsers[i].id);
+            optionElement.appendChild(document.createTextNode(allUsers[i].first_name));
+            selectElement.appendChild(optionElement);
+        }
+        document.getElementsByTagName("body")[0].appendChild(selectElement)
+
+
+    }
+}
+xhttp.open("GET", `https://reqres.in/api/users`, true);
+xhttp.send();
