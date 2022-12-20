7cdcc332cb49d859fb12e9ff583f50e91e8acd57
tarikwaleed <takizee53@gmail.com>
2022-12-20 18:21:09 +0200

    little changes

--- assignment4/2.js
+++ assignment4/2.js
@@ -1,11 +1,12 @@
 // window.onload = () => {
 var heading = document.createElement("h1")
-heading.innerHTML = "Colored Array"
+heading.innerHTML = "Colored Array "
 document.body.appendChild(heading)
 var colorArray = ['red', 'green', 'blue', 'orange', 'yellow', 'brown', 'gray', 'lightgray', 'gold']
 for (let i = 0; i < 7; i++) {
     var randomNumber = Math.floor(Math.random() * 10)
     var div = document.createElement("div")
+    div.className = "colored-array"
     div.style.backgroundColor = colorArray[randomNumber]
     div.style.width = "100px"
     div.style.height = "100px"
@@ -14,3 +15,14 @@ for (let i = 0; i < 7; i++) {
 }
 // };
 
+
+function changeColor() {
+    var colorArray = ['red', 'green', 'blue', 'orange', 'yellow', 'brown', 'gray', 'lightgray', 'gold']
+    var divs = document.getElementsByClassName("colored-array")
+    for (var i = 0; i < divs.length; i++) {
+        var randomNumber = Math.floor(Math.random() * 10)
+        divs[i].style.backgroudColor = colorArray[randomNumber];
+    }
+
+
+}
